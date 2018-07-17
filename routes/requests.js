4b6f3df61cb70628780f8b43a73ad9681cdf4529
tareksalem <tarekpopp@gmail.com>
2018-07-17 05:11:42 +0200

    re

--- routes/requests.js
+++ routes/requests.js
@@ -5,26 +5,29 @@ const http = require("http");
 // make a router for posting request data
 router.post("/", (req, res) => {
     // make some stuff here
-    var data = req.data;
-    var userNumber = data.userref;
-
+    var body = req.body;
+    var userNumber = body.userref;
     var request = http.request({
             host: "jawalsms.net",
-            path: `/httpSmsProvider.aspx?username=MgsalaCo&password=Project@2018&mobile=${userNumber}&unicode=E&message=رسالة تحقق&sender=MgsalaCo-ADg`,
+            path: encodeURI(`/httpSmsProvider.aspx?username=MgsalaCo&password=Project@2018&mobile=${userNumber}&unicode=E&message=رسالة تحقق&sender=MgsalaCo-AD`),
             port: 80,
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             }
-        }, function (res) {
-        res.on("data", function (d) {
-            console.log(d.toString());
+        }, function (response) {
+        response.on("data", function (d) {
+            var status = Number.parseInt(d.toString());
+            if (status === 1010) {
+                res.json({message: 'message sent successfully'});
+            } else {
+                res.json({message: 'error happened'});
+            }
         })
     });
     request.end();
     request.on("error", function (err) {
-        console.log("error is", err);
+        res.json({message: 'error happened'});
     })
-
 })
 module.exports = router;
