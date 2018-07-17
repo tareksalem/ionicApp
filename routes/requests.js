7c4106208070239a5846e0606e913b8411d587f5
tareksalem <tarekpopp@gmail.com>
2018-07-17 21:01:47 +0200

    qr

--- routes/requests.js
+++ routes/requests.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const http = require("http");
+const firebase = require("../firebase");
+const database = firebase.database();
 
 // make a router for posting request data
 router.post("/", (req, res) => {
     // make some stuff here
     var body = req.body;
-    var userNumber = body.userref;
+    var userref = body.userref || "";
+    var R_time = body.R_time || "";
+    var R_date = body.R_date || "";
+    var D_date = body.D_date || "";
+    var D_time = body.D_time || "";
+    var R_shift = body.R_shift || "";
+    var D_shift = body.D_shift || "";
+    var service = body.service || {};
+    var timeStamp = body.timeStamp || "";
+    var MapLongitude = body.MapLongitude || "";
+    var MapLatitude = body.MapLatitude || "";
+    var Notes = body.Notes || "";
     var request = http.request({
             host: "jawalsms.net",
-            path: encodeURI(`/httpSmsProvider.aspx?username=MgsalaCo&password=Project@2018&mobile=${userNumber}&unicode=E&message=رسالة تحقق&sender=MgsalaCo-AD`),
+            path: encodeURI(`/httpSmsProvider.aspx?username=MgsalaCo&password=Project@2018&mobile=${userref}&unicode=E&message=رسالة تحقق&sender=MgsalaCo-AD`),
             port: 80,
             method: "POST",
             headers: {
@@ -19,9 +32,23 @@ router.post("/", (req, res) => {
         response.on("data", function (d) {
             var status = Number.parseInt(d.toString());
             if (status === 1010) {
-                res.json({message: 'message sent successfully'});
+                // save data to firebase
+                database.ref("/requests/" + `-${Math.random().toString(36).substr(2, 30)}${Math.random().toString(36).substr(2, 30)}`).set({
+                    userref: userref,
+                    R_date: R_date,
+                    D_date: D_date,
+                    R_time: R_time,
+                    D_time: D_time,
+                    D_shift: D_shift,
+                    R_shift: R_shift,
+                    service: service,
+                    MapLatitude: MapLatitude,
+                    MapLongitude: MapLongitude,
+                    Notes: Notes
+                })
+                return res.json({message: 'message sent successfully'});
             } else {
-                res.json({message: 'error happened'});
+                return res.json({message: 'error happened'});
             }
         })
     });
